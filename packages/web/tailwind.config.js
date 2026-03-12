8f58b8282222964da2de07aa3305d06d61e56f90
Volobuev Dmitrii <simon100500@yandex.ru>
2026-03-12 14:30:22 +0300

    feat(web): update Tailwind CSS configuration to use ES module syntax
    - Imported tailwindcss-animate as a module.
    - Updated the Tailwind CSS configuration to use `export default` syntax.
    - Ensured plugins are referenced correctly in the configuration.

--- packages/web/tailwind.config.js
+++ packages/web/tailwind.config.js
@@ -1,5 +1,7 @@
+import tailwindcssAnimate from "tailwindcss-animate";
+
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+const config = {
   darkMode: ["class"],
   content: [
     "./index.html",
@@ -63,5 +65,7 @@ module.exports = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [tailwindcssAnimate],
 };
+
+export default config;
