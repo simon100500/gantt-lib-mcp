79c160545401568e9c8f4b1aacc10c7ea7fc6437
Volobuev Dmitrii <simon100500@yandex.ru>
2026-03-25 20:29:34 +0300

    feat(26-02): add sitemap endpoint

---
+++ packages/site/src/pages/sitemap.xml.ts
@@ -0,0 +1,21 @@
+const routes = ["/", "/features", "/faq", "/privacy", "/terms"];
+const siteUrl = "https://getgantt.ru";
+
+export const GET = () => {
+  const body = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${routes
+  .map(
+    (route) => `  <url>
+    <loc>${siteUrl}${route}</loc>
+  </url>`,
+  )
+  .join("\n")}
+</urlset>`;
+
+  return new Response(body, {
+    headers: {
+      "Content-Type": "application/xml; charset=utf-8",
+    },
+  });
+};
