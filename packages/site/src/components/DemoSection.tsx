27a7777f3c64eaeacfc679f6649a39be5958f14a
Volobuev Dmitrii <simon100500@yandex.ru>
2026-04-13 18:30:14 +0300

    fix: update Gantt chart description for clarity
    - modified the description in the DemoSection component to specify "Ганта" for better context and understanding of the Gantt chart feature.

--- packages/site/src/components/DemoSection.tsx
+++ packages/site/src/components/DemoSection.tsx
@@ -285,7 +285,7 @@ export default function DemoSection() {
               <br className="hidden sm:block" /> Легко управлять
             </h2>
             <p className="mx-auto mt-8 max-w-2xl text-base leading-7 text-slate-500 sm:text-xl sm:leading-8">
-              Новый стандарт графиков. Просто, красиво, онлайн.
+              Новый стандарт графиков Ганта. Просто, красиво, онлайн.
             </p>
 
             <div className="mt-8">
